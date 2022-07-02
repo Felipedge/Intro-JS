022a36d65772fa7c71e580e5ad308b261c61255a
Felipe P <102331600+Felipedge@users.noreply.github.com>
2022-07-02 04:00:17 -0400

    Add files via upload

---
+++ assest/js/scrip.js
@@ -0,0 +1,22 @@
+const button = document.querySelector("button");
+
+const multTotal = () => {
+    
+    const cantidad = document.querySelector("#cantidad").value;
+    const precio = 1199990
+    const color = document.querySelector('#color');
+
+    if (isNaN(cantidad)) {
+        resultado = "Es necesario introducir un numero válidos";
+    } 
+    else {
+        const cantidadTotal = +cantidad * precio;
+        resultado = cantidadTotal;
+    }
+    document.querySelector("#precioFinal").textContent = resultado;
+    document.querySelector('#cantidadFinal').textContent = cantidad;
+    document.querySelector('#colorFinal').textContent = color;
+    color.style.backgroundColor = colorImput.value;
+}
+    
+button.addEventListener("click", multTotal);
